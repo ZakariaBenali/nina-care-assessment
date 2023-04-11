67d354e6c901a1c5397eb05b9e23e8f3fa0479c7
ZakariaBenali <benalizak97@gmail.com>
2023-04-11 15:24:35 +0000

    feat: add filter scope to combine the previously added locale scopes with request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,6 +12,6 @@ class UserController extends Controller
      */
     public function index()
     {
-        return User::with('posts')->get();
+        return User::filter()->with('posts')->get();
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -56,6 +56,28 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
     }
 
+    public function scopeFilter(Builder $builder) {
+        $builder->when(request()->has('gender'), function($query) {
+            return $query->ofGender(request()->gender);
+        });
+
+        $builder->when(request()->has('minAge') || request()->has('maxAge'), function($query) {
+            return $query->ageBetween(request()->minAge, request()->maxAge);
+        });
+
+        $builder->when(request()->has('name'), function($query) {
+            return $query->named(request()->name);
+        });
+
+        $builder->when(request()->has('name'), function($query) {
+            return $query->named(request()->name);
+        });
+
+        $builder->when(request()->has('post_title'), function($query) {
+            return $query->postTile(request()->post_title);
+        });
+    }
+
     public function scopeOfGender(Builder $query, string $gender) {
         $query->where('gender', $gender);
     }
